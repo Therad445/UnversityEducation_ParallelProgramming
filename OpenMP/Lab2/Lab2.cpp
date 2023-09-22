484860f912c5ff5312e8fc8f6776281fd6101190
Радмир Исламов <therad445@outlook.com>
2023-09-23 02:42:18 +0300

    Final changes Lab2 and Reports

--- OpenMP/Lab2/Lab2.cpp
+++ OpenMP/Lab2/Lab2.cpp
@@ -1,6 +1,10 @@
 ﻿// Lab2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
+/*
+Задание. Выполнить Пример для различных значений параметров NMAX и LIMIT, замеряя время выполнения, результаты занести в отчет. Выделить такие NMAX при LIMIT, при которых совпадает время выполнения многопоточной программы и однопоточной.
+*/
+
 #include <iostream>
 #include <ctime>
 #include <omp.h>
@@ -8,13 +12,14 @@
 
 using namespace std;
 
-const int NMAX = 100;
-const int LIMIT = 10;
+const int NMAX = 300;
+const int LIMIT = 400;
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	unsigned int start_time = clock(); // начальное время
+
+	unsigned int start_time1 = clock(); // начальное время
 	srand(time(0)); // автоматическая рандомизация
 
 	{
@@ -26,9 +31,9 @@ int main()
 			for (j = 0; j < NMAX; j++)
 				a[i][j] = i + j;
 
-#pragma omp parallel shared(a) if (NMAX>LIMIT)
+		#pragma omp parallel shared(a) if (NMAX>LIMIT)
 		{
-#pragma omp for private(i,j,sum) 
+		#pragma omp for private(i,j,sum) 
 			for (i = 0; i < NMAX; i++)
 			{
 				sum = 0;
@@ -39,10 +44,41 @@ int main()
 		}
 	}
 
-	unsigned int end_time = clock(); // конечное время
-	unsigned int search_time = end_time - start_time; // искомое время
+	unsigned int end_time1 = clock(); // конечное время
+	unsigned int search_time1 = end_time1 - start_time1; // искомое время
 
-	cout << "Задача выполнена с использованием OpenMP: " << search_time;
+	//cout << "Задача выполнена с использованием OpenMP: " << search_time1;
+	//cout << endl;
+
+
+	unsigned int start_time2 = clock(); // начальное время
+	srand(time(0)); // автоматическая рандомизация
+
+	{
+	int i, j;
+	float sum;
+	float a[NMAX][NMAX];
+
+	for (i = 0; i < NMAX; i++)
+		for (j = 0; j < NMAX; j++)
+			a[i][j] = i + j;
+
+		for (i = 0; i < NMAX; i++)
+		{
+			sum = 0;
+			for (j = 0; j < NMAX; j++)
+				sum += a[i][j];
+			printf("Сумма элементов строки %d равна %f\n", i, sum);
+		}
+
+	unsigned int end_time2 = clock(); // конечное время
+	unsigned int search_time2 = end_time2 - start_time2; // искомое время
+
+	cout << "Задача выполнена с использованием OpenMP: " << search_time1;
 	cout << endl;
+
+	cout << "Задача выполнена без использованием OpenMP: " << search_time2;
+	cout << endl;
+	}
 
 }
