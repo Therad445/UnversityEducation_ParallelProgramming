2dc32fb188a2b88818fb1180678daf46e5ab9608
Радмир Исламов <therad445@outlook.com>
2023-12-03 22:52:05 +0300

    Edits Lab3

--- MPI/Lab3/Lab3.cpp
+++ MPI/Lab3/Lab3.cpp
@@ -15,9 +15,12 @@ int main(int argc, char** argv) {
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	int* globaldata = NULL;
+	int localdata;
 	mode[0] = 1;
 	while (mode[0])
 	{
+		globaldata = malloc(size * sizeof(int));
 		arr[0] = rand() % 101 - 1;
 		if (rank == 0) {
 			MPI_Allreduce(arr, res, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
@@ -34,9 +37,20 @@ int main(int argc, char** argv) {
 			MPI_Allreduce(arr, res, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
 			MPI_Bcast(mode, 1, MPI_INT, 0, MPI_COMM_WORLD);
 		}
+
+		MPI_Scatter(globaldata, 1, MPI_INT, &localdata, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+		printf("Processor %d has data %d\n", rank, localdata);
+		localdata *= 2;
+		printf("Processor %d doubling the data, now has %d\n", rank, localdata);
+
+		MPI_Gather(&localdata, 1, MPI_INT, globaldata, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
 	}
 	free(arr);
 	free(res);
 	MPI_Finalize();
 	return 0;
-} Использовать Gather и Scatter
+} 
+Использовать Gather и Scatter
+https ://stackoverflow.com/questions/13867809/how-are-mpi-scatter-and-mpi-gather-used-from-c
